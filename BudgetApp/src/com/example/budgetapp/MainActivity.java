1d540077a249a240d306aa0ec970bfa359572fbb
Kayla Boyer <kaylaboyer5@gmail.com>
2013-11-06 11:29:02 -0500

    added popup box fragment

--- BudgetApp/src/com/example/budgetapp/MainActivity.java
+++ BudgetApp/src/com/example/budgetapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.budgetapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.DialogFragment;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -19,4 +22,23 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
-}
+	public void onClickAdd(View v){
+		//create dialog fragment when button is clicked 
+		DialogFragment newFragment = new ScannerFragment();
+	    newFragment.show(getFragmentManager(), "missiles");
+		
+	}
+	
+    public void onClickScanner(View v){
+    	Toast.makeText(this, "Scanner", Toast.LENGTH_LONG).show();
+    	
+    }
+    public void onClickManual(View v){	
+    	Toast.makeText(this, "Manual", Toast.LENGTH_LONG).show();
+    	
+    }
+}    
+
+
+
+
---
+++ BudgetApp/src/com/example/budgetapp/ScannerFragment.java
@@ -0,0 +1,37 @@
+package com.example.budgetapp;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
+
+public class ScannerFragment extends DialogFragment {
+	
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+
+         builder.setView(inflater.inflate(R.layout.inventory_option_box, null))   
+       
+               .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                   public void onClick(DialogInterface dialog, int id) {
+                       // User cancelled the dialog
+                   }
+               });
+        	
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+    
+
+}
