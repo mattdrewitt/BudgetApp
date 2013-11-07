ebeba7a0aee933f054204ea193dbc767df51f119
Matthew Drewitt <m.drewitt@hotmail.com>
2013-11-07 11:31:12 -0500

    Add dialog box for choosing manual or barcode.

--- BudgetApp/src/com/example/budgetapp/InventoryActivity.java
+++ BudgetApp/src/com/example/budgetapp/InventoryActivity.java
@@ -5,6 +5,7 @@ import com.google.zxing.integration.android.IntentResult;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.DialogFragment;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
@@ -13,6 +14,8 @@ import android.widget.Toast;
 
 public class InventoryActivity extends Activity implements OnClickListener {
 	int buttonClicked;
+	DialogFragment newFragment;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -49,7 +52,7 @@ public class InventoryActivity extends Activity implements OnClickListener {
 				}
 			}
 		}
-		else{
+		else {
 			//invalid scan data or scan canceled
 			Toast toast = Toast.makeText(getApplicationContext(), 
 					"No scan data received!", Toast.LENGTH_SHORT);
@@ -60,16 +63,35 @@ public class InventoryActivity extends Activity implements OnClickListener {
 	@Override
 	public void onClick(View v) {
 		if (v.getId() == R.id.btnAddInventory || v.getId() == R.id.btnUpdateInventory) {
+			//create dialog fragment when button is clicked 
+			newFragment = new ScannerFragment();
+			newFragment.show(getFragmentManager(), "missiles");
+			
 			// Set int for button tracking
 			buttonClicked = v.getId();
-			
-			// Instantiate ZXing integration class
-			IntentIntegrator scanIntegrator = new IntentIntegrator(this);
-			
-			// Start scanning
-			scanIntegrator.initiateScan();
 		}
 	}
 	
-
+	public void onClickScanner(View v) {
+		newFragment.dismiss();
+		
+		// Instantiate ZXing integration class
+		IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+		
+		// Start scanning
+		scanIntegrator.initiateScan();
+	}
+	
+	public void onClickManual(View v) {
+		newFragment.dismiss();
+		
+		switch(buttonClicked) {
+		case R.id.btnAddInventory:
+			startActivity(new Intent(this, AddInventoryActivity.class));
+			break;
+		case R.id.btnUpdateInventory:
+			startActivity(new Intent(this, UpdateInventoryActivity.class));
+			break;
+		}
+	}
 }
